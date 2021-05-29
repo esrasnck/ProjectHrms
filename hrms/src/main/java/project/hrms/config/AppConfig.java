8931e0c8d3b5dff530eec8def898d33f13509d4a
esrasnck <esrasancak@hotmail.com>
2021-05-30 00:28:49 +0300

    Commit

--- hrms/src/main/java/project/hrms/config/AppConfig.java
+++ hrms/src/main/java/project/hrms/config/AppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {   // service tool mantığı
 	
+	
 	@Bean
 	public ModelMapper modelMapper() {
 			
